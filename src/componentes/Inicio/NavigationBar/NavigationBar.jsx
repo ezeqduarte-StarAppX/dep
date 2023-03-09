d0d6ddb9186d2fa9c1674422897d8c7a298addef
Ezequiel Duarte <ezequielduartesax@gmail.com>
2023-03-09 17:27:19 -0300

    navbar con actives lista

--- src/componentes/Inicio/NavigationBar/NavigationBar.jsx
+++ src/componentes/Inicio/NavigationBar/NavigationBar.jsx
@@ -1,38 +1,44 @@
 import { Menu } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import { buttons } from "../../../data/buttons-navegation-bar";
 import "./navigationbar.css";
 
 export default function NavigationBar() {
+  const location = useLocation();
+
+  const [url, setUrl] = useState(null);
+  useEffect(() => {
+    setUrl(location.pathname);
+  }, [location]);
+
   return (
     <div className="navigation-bar">
       <img src="./svg/menu.svg" alt="" />
       <div className="navigation-bar-icons">
-        <div>
-          <img src="./svg/perfil.svg" alt="" />
-          <p>PERFIL</p>
-        </div>
-        <div>
-          <img src="./svg/reservas.svg" alt="" />
-           <p>RESERVAS</p>
-        </div>
-        <div>
-          <img src="./svg/home.svg" alt="" />
-           <p>HOME</p>
-        </div>
-        <div>
-          <img src="./svg/store.svg" alt="" />
-           <p>STORE</p>
-        </div>
-        <div>
-          <img src="./svg/chat.svg" alt="" />
-           <p>CHATS</p>
-        </div>
-        <div>
-          <img src="./svg/settings.svg" alt="" />
-           <p>CONFIG</p>
-        </div>
+        {buttons.map((button) =>
+          url === button.rute ? (
+            <NavLink to={button.rute} style={{ textDecoration: "none" }}>
+              <div className="active-button">
+                <img
+                  className="active-img"
+                  src={button.imgActive}
+                  alt={button.button}
+                />
+                <p className="active-p">{button.button}</p>
+              </div>
+            </NavLink>
+          ) : (
+            <NavLink to={button.rute} style={{ textDecoration: "none" }}>
+              <div>
+                <img src={button.img} alt={button.button} />
+                <p>{button.button}</p>
+              </div>
+            </NavLink>
+          )
+        )}
       </div>
-      <img  src="./svg/logout.svg" alt="logout" />
+      <img src="./svg/logout.svg" alt="logout" />
     </div>
   );
 }
---
+++ src/data/buttons-navegation-bar.js
@@ -0,0 +1,38 @@
+export const buttons = [
+    {
+      button: "PERFIL",
+      rute: "/perfil",
+      img: "./svg/perfil.svg",
+      imgActive: "./svg/perfil-active.svg",
+    },
+    {
+      button: "RESERVAS",
+      rute: "/reservas",
+      img: "./svg/reservas.svg",
+      imgActive: "./svg/reservas-active.svg",
+    },
+    {
+      button: "HOME",
+      rute: "/",
+      img: "./svg/home.svg",
+      imgActive: "./svg/home-active.svg",
+    },
+    {
+      button: "STORE",
+      rute: "/store",
+      img: "./svg/store.svg",
+      imgActive: "./svg/store-active.svg",
+    },
+    {
+      button: "CHATS",
+      rute: "/chats",
+      img: "./svg/chat.svg",
+      imgActive: "./svg/chat-active.svg",
+    },
+    {
+      button: "CONFIG",
+      rute: "/config",
+      img: "./svg/settings.svg",
+      imgActive: "./svg/settings-active.svg",
+    },
+  ];
