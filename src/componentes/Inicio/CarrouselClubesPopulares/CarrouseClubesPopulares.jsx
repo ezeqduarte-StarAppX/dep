4cc72699465f74b4f5e0543f6cb5db07d533e8b7
Ezequiel Duarte <ezequielduartesax@gmail.com>
2023-03-10 14:55:59 -0300

    carrouseles home listos

--- src/componentes/Inicio/CarrouselClubesPopulares/CarrouseClubesPopulares.jsx
+++ src/componentes/Inicio/CarrouselClubesPopulares/CarrouseClubesPopulares.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardCarrousel from "../CardCarrousel/CardCarrousel";
 import { motion } from "framer-motion";
 import "./carrouselClubesPopulares.css";
@@ -7,13 +7,32 @@ import { auto } from "@popperjs/core";
 import CardPopularClubes from "../../TypesCards/PopularClubes/CardPopularClubes";
 
 export default function CarrouseClubesPopulares({ components }) {
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  function scrollCarrousel() {
+    console.log(window.screen.width);
+    if (window.screen.width > 1700) {
+      setScrollLeft(-970);
+      console.log("hola soy grande");
+    }
+
+    if (window.screen.width > 900 && window.screen.width < 1700) {
+      setScrollLeft(-1300);
+      console.log("hola soy chico");
+    }
+  }
+
+  useEffect(() => {
+    scrollCarrousel();
+  }, []);
+
   return (
     <>
       <motion.div className="slider-container-popular-clubes">
         <motion.div
           className="slider-popular-clubes"
           drag="x"
-          dragConstraints={{ right: 0, left: "-1271px" }}
+          dragConstraints={{ right: 0, left: scrollLeft }}
         >
           {components.map((club) => (
             <motion.div className="item">
--- src/componentes/Inicio/CarrouselHome/CarrouselHome.jsx
+++ src/componentes/Inicio/CarrouselHome/CarrouselHome.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CardCarrousel from "../CardCarrousel/CardCarrousel";
 import { motion } from "framer-motion";
 import "./carrouselHome.css";
@@ -6,13 +6,30 @@ import { clubes } from "../../../data/arrayPruebaCarrousel";
 import { auto } from "@popperjs/core";
 
 export default function CarrouselHome({ components, card }) {
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  function scrollCarrousel() {
+    console.log(window.screen.width);
+    if (window.screen.width > 1700) {
+      setScrollLeft(-1400);
+    }
+
+    if (window.screen.width > 900 && window.screen.width < 1700) {
+      setScrollLeft(-1730);
+    }
+  }
+
+  useEffect(() => {
+    scrollCarrousel();
+  }, []);
+
   return (
     <>
       <motion.div className="slider-container">
         <motion.div
           className="slider"
           drag="x"
-          dragConstraints={{ right: 0, left: -1790 }}
+          dragConstraints={{ right: 0, left: scrollLeft }}
         >
           {components.map((image) => (
             <motion.div className="item">
@@ -22,7 +39,6 @@ export default function CarrouselHome({ components, card }) {
           ))}
         </motion.div>
       </motion.div>
-      
     </>
   );
 }
